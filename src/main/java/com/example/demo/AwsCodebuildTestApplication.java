0adf3bce2ba4ba16c2fcde7413d1bdfcb7c4a29a
kishor <kishor.jabegu5@gmail.com>
2020-10-13 21:07:42 +1100

    first commit

---
+++ src/main/java/com/example/demo/AwsCodebuildTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AwsCodebuildTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AwsCodebuildTestApplication.class, args);
+	}
+
+}
